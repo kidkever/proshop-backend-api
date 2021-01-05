9c5e79a0a49b4755bb2f845bbf1a6aa349ee21ff
Mazen Mohamed <kidkever@yahoo.com>
2021-01-05 05:48:30 +0200

    checkout pt2 sec10

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { addOrderItems } from "../controllers/orderController.js";
+import {
+  addOrderItems,
+  getMyOrders,
+  getOrderById,
+  updateOrderToPaid,
+} from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", protect, addOrderItems);
+router.get("/myorders", protect, getMyOrders);
+router.get("/:id", protect, getOrderById);
+router.put("/:id/pay", protect, updateOrderToPaid);
 
 export default router;
