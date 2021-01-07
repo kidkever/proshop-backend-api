2b1de0588c6a89b9f820203a8e8cb8506e4349c3
Mazen Mohamed <kidkever@yahoo.com>
2021-01-07 04:33:51 +0200

    admin screen pt1 sec11

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,26 @@
 import express from "express";
 import {
   authUser,
+  deleteUser,
+  getUserById,
   getUserProfile,
+  getUsers,
   registerUser,
+  updateUser,
   updateUserProfile,
 } from "../controllers/userController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", registerUser);
 router.post("/login", authUser);
+
+router.get("/", protect, admin, getUsers);
+router.get("/:id", protect, admin, getUserById);
+router.put("/:id", protect, admin, updateUser);
+router.delete("/:id", protect, admin, deleteUser);
+
 router.get("/profile", protect, getUserProfile);
 router.put("/profile", protect, updateUserProfile);
 
